c0b62c8b400261f5e6011ed0496987f01ba5d225
dvbreva <jump.the.stars@abv.bg>
2019-09-07 17:11:14 +0300

    Initial commit.

---
+++ ElectiveCourses/ElectiveCourses/Models/ElectiveCourse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectiveCourses.Models
+{
+    public class ElectiveCourse
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Capacity { get; set; }
+
+        [Required]
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ ElectiveCourses/ElectiveCourses/Models/ElectiveCoursesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElectiveCourses.Models
+{
+    public class ElectiveCoursesDbContext : DbContext
+    {
+        public ElectiveCoursesDbContext(DbContextOptions<ElectiveCoursesDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
