ccfa930338e96ee7ae48b4c890e2d07cae41ff21
dvbreva <jump.the.stars@abv.bg>
2019-09-24 23:59:36 +0300

    Movies: inconsistency fix.

--- movie-catalog/src/app/app.module.ts
+++ movie-catalog/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieService } from './services/movie.service';
 import { HttpClientModule } from '@angular/common/http';
-import { MovieDetailsComponent } from './services/movie-details/movie-details.component';
 import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieDetailsComponent,
     MovieComponent
   ],
   imports: [
